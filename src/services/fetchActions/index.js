cd5f2f7d0605cc59af3b46f4dcb864a4557a17ec
esionascimento <esio.392@protonmail.com>
2021-12-08 13:34:37 -0400

    remv

--- src/services/fetchActions/index.js
+++ src/services/fetchActions/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-require("dotenv").config();
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = "https://desafio-ser-educacional-back.herokuapp.com";
 const token = localStorage.getItem('token');
 
 export const newCadastro = (user) => {
