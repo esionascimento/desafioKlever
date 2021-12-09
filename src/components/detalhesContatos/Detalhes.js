37f37626879ce6a7715efbae14b11c4dd564fa49
esionascimento <esio.392@protonmail.com>
2021-12-09 12:57:01 -0400

    refatora codigo, implementando modal

---
+++ src/components/detalhesContatos/Detalhes.js
@@ -0,0 +1,71 @@
+import React, { useEffect } from 'react';
+import { Modal } from 'antd';
+import { dashboard } from '../../services/fetchActions';
+import { useSelector, useDispatch } from 'react-redux';
+import { VisibleModaldetalhes } from '../../store/visibleModal/visibleModal.actions';
+
+export const Detalhes = (chaveVisible) => {
+  const dispatch = useDispatch();
+  const { contato } = useSelector(state => state.dashboard);
+  const { visibleModalDetalhes } = useSelector(state => state.visibleModal);
+
+  const [data, setData ] = React.useState([]);
+  const [visible, setVisible] = React.useState(false);
+  const [confirmLoading, setConfirmLoading] = React.useState(false);
+
+  useEffect(() => {
+    setVisible(visibleModalDetalhes);
+  }, [visibleModalDetalhes]);
+
+  const handleOk = () => {
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+      dispatch(VisibleModaldetalhes(false));
+    }, 500);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+    dispatch(VisibleModaldetalhes(false));
+  };
+
+  useEffect(() => {
+    dashboard().then((aux) => {
+      if (aux.data["length"] > 0) {
+        const salve = aux.data[0].data;
+        salve.map((data, index) => contato === salve[index].nome &&
+            setData(data)
+        )
+      }
+    });
+  },[contato]);
+
+  function formDetalhes() {
+    return (
+      <form>
+        <p>Nome: <span>{data.nome}</span></p>
+        <p>Sobrenome: <span>{data.sobrenome}</span></p>
+        <p>Telefone: <span>{data.telefone}</span></p>
+        <p>Email: <span>{data.email}</span></p>
+        <p>Data de nascimento: <span>{data.dataNascimento}</span></p>
+        <p>Endereço: <span>{data.endereco}</span></p>
+      </form>
+    )
+  }
+
+  return (
+    <>
+      <Modal
+        title={`Detalhes do(a) ${data.nome}`}
+        visible={visible}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+        >
+        {formDetalhes()}
+      </Modal>
+    </>
+  );
+};
