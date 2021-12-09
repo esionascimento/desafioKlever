b80edf81ed498c645e7c8f7bff3c05dfc0589049
esionascimento <esio.392@protonmail.com>
2021-12-09 12:58:19 -0400

    implementado redux para auxilixar no modal do detalhes do contato

--- src/components/nomesContatos/index.js
+++ src/components/nomesContatos/index.js
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { dashboard } from '../../services/fetchActions';
 import { useDispatch } from 'react-redux';
 import { dashboardDelete } from '../../services/fetchActions';
 import { DashboardSelectContato } from '../../store/dashboard/Dashboard.actions';
 import { message } from 'antd';
 import { Reload } from '../reload';
-
+import { Detalhes } from '../detalhesContatos/Detalhes';
+import 'antd/dist/antd.css';
 import { Input, Div } from './nomesContatos';
+import { VisibleModaldetalhes } from '../../store/visibleModal/visibleModal.actions';
 
 function NomesContatos() {
   const dispatch = useDispatch();
-  const [ data, setData ] = useState([]);
+  const [data, setData ] = useState([]);
 
   useEffect(() => {
     dashboard().then((aux) => {
@@ -23,6 +25,7 @@ function NomesContatos() {
 
   function onClick(e) {
     dispatch(DashboardSelectContato(e.target.name));
+    dispatch(VisibleModaldetalhes(true));
   }
 
   const RemoverContato = async (event) =>  {
@@ -47,12 +50,13 @@ function NomesContatos() {
                 name={`${or["nome"]}`}
                 value={`${or["nome"]}`}
                 />
-              <button>Lupa</button>
+              <button>Editar</button>
               <button name={`${or["nome"]}`} onClick={(e) => RemoverContato(e)}>Deletar</button>
             </Div>
         ))
         : <p>Nenhum contato</p>
       }
+      <Detalhes />
     </div>
   );
 }
