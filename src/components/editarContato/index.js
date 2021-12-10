f658536338f87d9fce31a8f4bceb06ae86d29509
esionascimento <esio.392@protonmail.com>
2021-12-10 18:48:38 -0400

    criado arquivo modal para usuario alterar contato

--- src/components/editarContato/index.js
+++ src/components/editarContato/index.js
@@ -1,10 +1,34 @@
-
+import React from 'react';
 import './editarContatoCss';
 
-export function EditarContato() {
+import { Modal } from 'antd';
+
+export const EditarContato = () => {
+  const [visible, setVisible] = React.useState(false);
+  const [confirmLoading, setConfirmLoading] = React.useState(false);
+
+  const handleOk = () => {
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
   return (
     <>
-      
+      <Modal
+        title="Title"
+        visible={visible}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+      </Modal>
     </>
   );
-}
+};
