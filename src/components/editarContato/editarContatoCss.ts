3e0b4b8171277d00f19c0ec055e48f44fbc60225
esionascimento <esio.392@protonmail.com>
2021-12-13 17:44:19 -0400

    criado arquivo de css styled-components

---
+++ src/components/editarContato/editarContatoCss.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  width: 350px;
+  @media(max-width: 500px) {
+    width: 100%;
+  }
+`;
+
+export const DivForm = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const Label = styled.label`
+  width: 100px;
+`;
